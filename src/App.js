346291a2a8cf59f950bb892e47e6654964ee5857
omarmt29 <omarmendezt29@gmail.com>
2022-05-09 18:53:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import app from './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        axios.get('https://randomuser.me/api/')
+            .then(function (response) {
+                setData(response.data.results[0]);
+                console.log(data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+    }, [loading])
+
+
+
+    return (
+        <>
+
+            <div className='container'>
+                <img src={data.picture.large} alt="" />
+                <h1><b>Fullname: </b>{data.name.first} {data.name.last}</h1>
+                <p><b>Email: </b>{data.email}</p>
+                <p><b>city: </b>{data.location.city}</p>
+                <p><b>street: </b>{data.location.street.name}</p>
+                <p><b>postcode: </b> {data.location.postcode}</p>
+                <p><b>state: </b> {data.location.state}</p>
+                <p><b>country: </b>{data.location.country}</p>
+
+                <button onClick={() => setLoading(!loading)}>Random user</button>
+
+
+            </div>
+
+
+        </>
+    );
 }
-
 export default App;
